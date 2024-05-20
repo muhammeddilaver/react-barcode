ae397732b2738d054a9b603a6ad8f263c428518a
muhammeddilaver <muhammed.dilaver@gmail.com>
2024-05-20 15:40:13 -0400

    barcode

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import BarcodeScanner from './components/BarcodeScanner';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -1,99 +1,42 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Quagga from 'quagga';
+import { BrowserMultiFormatReader, NotFoundException } from '@zxing/library';
 
 const BarcodeScanner = () => {
   const [scannedCode, setScannedCode] = useState(null);
   const videoRef = useRef(null);
 
   useEffect(() => {
-    Quagga.init({
-      inputStream: {
-        type: 'LiveStream',
-        target: videoRef.current, // Or '#yourElement' (optional)
-        constraints: {
-          width: 640,
-          height: 480,
-          facingMode: 'environment' // or user for the front camera
-        },
-      },
-      locator: {
-        patchSize: 'medium', // x-small, small, medium, large, x-large
-        halfSample: true,
-      },
-      decoder: {
-        readers: [
-          'code_39_reader',
-          'code_128_reader',
-          'ean_reader',
-          'ean_8_reader',
-        ],
-      },
-      locate: true,
-      debug: {
-        drawBoundingBox: true,
-        showFrequency: true,
-        drawScanline: true,
-        showPattern: true
-      }
-    }, (err) => {
-      if (err) {
+    const codeReader = new BrowserMultiFormatReader();
+    codeReader
+      .listVideoInputDevices()
+      .then(videoInputDevices => {
+        const firstDeviceId = videoInputDevices[0].deviceId;
+        codeReader
+          .decodeFromVideoDevice(firstDeviceId, videoRef.current, (result, err) => {
+            if (result) {
+              setScannedCode(result.getText());
+            }
+            if (err && !(err instanceof NotFoundException)) {
+              console.error(err);
+            }
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      })
+      .catch(err => {
         console.error(err);
-        return;
-      }
-      Quagga.start();
-    });
-
-    Quagga.onProcessed((result) => {
-      if (result) {
-        const drawingCtx = Quagga.canvas.ctx.overlay;
-        const drawingCanvas = Quagga.canvas.dom.overlay;
-
-        if (result.boxes) {
-          drawingCtx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
-          result.boxes.filter(box => box !== result.box)
-            .forEach(box => {
-              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: 'green', lineWidth: 2 });
-            });
-        }
-
-        if (result.box) {
-          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: 'blue', lineWidth: 2 });
-        }
-
-        if (result.codeResult && result.codeResult.code) {
-          drawingCtx.font = "24px Arial";
-          drawingCtx.fillStyle = "red";
-          drawingCtx.fillText(result.codeResult.code, 10, 20);
-        }
-      }
-    });
-
-    Quagga.onDetected((data) => {
-      const code = data.codeResult.code;
-      const accuracy = data.codeResult.decodedCodes
-        .filter(decoded => decoded.error !== undefined)
-        .reduce((sum, decoded) => sum + decoded.error, 0) / data.codeResult.decodedCodes.length;
-
-      // Adjust the accuracy threshold as needed
-      if (accuracy < 0.15) {
-        if (code.toString().length === 13) {
-          setScannedCode(code);
-        }
-        //Quagga.stop();
-      }
-      //Quagga.stop();
-    });
+      });
 
     return () => {
-      //Quagga.offDetected();
-      //Quagga.stop();
+      codeReader.reset();
     };
   }, []);
 
   return (
     <div>
       {scannedCode && <div>Scanned Code: {scannedCode}</div>}
-      <div ref={videoRef} style={{ width: '100%', height: 'auto' }}></div>
+      <video ref={videoRef} style={{ width: '100%', height: 'auto' }}></video>
     </div>
   );
 };
