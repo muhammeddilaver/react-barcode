2a10b8b25111c2de13b419fb1d30a3d963bd1d46
muhammeddilaver <muhammed.dilaver@gmail.com>
2024-05-20 14:26:55 -0400

    barcode

--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -17,7 +17,16 @@ const BarcodeScanner = () => {
         },
       },
       decoder: {
-        readers: ['code_128_reader', 'ean_reader', 'ean_8_reader', 'code_39_reader', 'code_39_vin_reader', 'upc_reader', 'upc_e_reader', 'i2of5_reader'],
+        readers: [
+          'code_39_reader',
+          'code_128_reader', 
+          'ean_reader', 
+          'ean_8_reader', 
+          'code_39_vin_reader', 
+          'upc_reader', 
+          'upc_e_reader', 
+          'i2of5_reader'
+        ],
       },
     }, (err) => {
       if (err) {
